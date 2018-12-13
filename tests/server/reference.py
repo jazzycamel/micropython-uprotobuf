b5a7148f893a68bf74951667bca32c4d5720cd25
Rob Kent <rob@33216.printrs.lifesci.dundee.ac.uk.printers.lifesci.dundee.ac.uk>
2018-12-13 14:34:35 +0000

    Removing stale files

--- tests/server/reference.py
+++
@@ -1,7 +0,0 @@
-from tests_pb2 import Test1 as Test1Message
-
-if __name__=="__main__":
-    m=Test1Message()
-    #m.Enum=Test1Message.ValueB
-    m.String="My hovercraft is full of eels!"
-    print(m.SerializeToString())
